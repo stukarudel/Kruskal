d652488e1cb91c63d8339dac8b3b95cff15ca257
stukarudel <adrianmenard@hotmail.com>
2014-12-01 18:49:55 +0000

    adding graph input file & code to load the graph in Kruskal.main

--- src/Kruskal.java
+++ src/Kruskal.java
@@ -1,3 +1,6 @@
+
+import java.util.Iterator;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -15,9 +18,36 @@ public class Kruskal {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
-        //comment
-    }
+   public static void main(String[] args) {
+          SimpleGraph G;
+          G = new SimpleGraph(); 
+          GraphInput.LoadSimpleGraph(G, "graph1.txt");
+          DisplayGraph(G);
+   }
+   
+   public static void DisplayGraph(SimpleGraph G) {
+       //Method to print the graph to the console.
+       System.out.println(G.numVertices() + " verticies in graph.");
+       System.out.println(G.numEdges() + " edges in graph.");
+       Iterator i; 
+       System.out.println("Iterating through vertices...");
+        for (i= G.vertices(); i.hasNext(); ) {
+            Vertex v = (Vertex) i.next();
+            System.out.println("found vertex " + v.getName());
+        }
+
+        System.out.println("Iterating through adjacency lists...");
+        for (i= G.vertices(); i.hasNext(); ) {
+            Vertex v = (Vertex) i.next();
+            System.out.println("Vertex "+v.getName());
+            Iterator j;
+            
+            for (j = G.incidentEdges(v); j.hasNext();) {
+                Edge e = (Edge) j.next();
+                System.out.println("  found edge ((" + e.getFirstEndpoint().getName() + "," + e.getSecondEndpoint().getName() + "), weight:" + e.getData() + ")");
+            }
+        }
+
+   }
     
 }
